29bdecb1addbf921f4c5acfc3f666a2564de09a0
tomatomatomato93 <57449740+tomatomatomato93@users.noreply.github.com>
2020-07-06 04:52:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,72 @@
-'use script'
-{
-    const btn = document.getElementById('btn');
-    const btn2 = document.getElementById('btn2');
-    const today = document.getElementById('today');
-
-    const todays = new Date();
-    // const  = today.getFullYear();
-    // const  = todays.getMonth();
-    // const  = todays.getDate();
-    
-    today.textContent = `${todays.getFullYear()}年${todays.getMonth() + 1}月${todays.getDate()}日の`;
-
-    startbtn.addEventListener('click', ()=>{
-        const results = ['朝食','昼食','夕食','おやつ'];
-        btn.textContent = results[Math.floor(Math.random() * results.length)];
-
-        const n = Math.random() * 10;
-        // btn2.textContent(results2);
-        if(n < 4){
-            btn2.textContent = 'マンゴー';
-        } else if(n < 7){
-            btn2.textContent = 'キウイ';
-        }else if(n < 8){
-            btn2.textContent = '桃';
-        } else {
-            btn2.textContent = 'メロン';
-        }
-    });
-
-    btn.addEventListener('click', ()=>{
-        const results = ['朝食','昼食','夕食','おやつ'];
-        btn.textContent = results[Math.floor(Math.random() * results.length)];
-    });
-
-    btn2.addEventListener('click', ()=>{
-        const results2 = ['マンゴー','キウイ','桃','メロン'];
-        btn2.textContent = results2[Math.floor(Math.random() * results2.length)];
-    });
+body{
+    background-color: #ffe4c4;
+    /* height: 50%;
+    width: 100%; */
 }
+
+.block{
+    /* display: inline-block; */
+    text-align: center;
+}
+h1 {
+    font-size: 30px;
+    font-weight: normal;
+    display: inline-block;
+}
+
+#btn {
+    width: 300px;
+    height: 200px;
+    background: #cccc00;
+    border-radius: 50%;
+    margin: 50px auto;
+    text-align: center;
+    line-height:200px;
+    font-size: 40px;
+    display: inline-block;
+    cursor: pointer;
+    -webkit-user-select: none;
+}
+#btn:hover{
+    opacity: 0.8;
+}
+#btn:active{
+    position: relative;
+    top: 2px;
+}
+
+#btn2 {
+    width: 300px;
+    height: 200px;
+    background: #99cc00;
+    border-radius: 50%;
+    margin: 50px auto;
+    text-align: center;
+    line-height:200px;
+    font-size: 40px;
+    display: inline-block;
+    cursor: pointer;
+    -webkit-user-select: none;
+}
+#btn2:hover{
+    opacity: 0.8;
+}
+#btn2:active{
+    position: relative;
+    top: 2px;
+}
+
+.start{
+    text-align: center;
+}
+#startbtn{
+    background-color: #ffff88;
+    border-radius: 10%;
+    cursor: pointer;
+    font-size: 30px;
+}
+#startbtn:active{
+    position: relative;
+    top: 2px;
+}
+
